0e9285b11061b7a0859e4e9552295b4a6a449a35
Matthieu Meunier <matth.meun@hotmail.com>
2017-03-13 16:41:33 +0100

    fonctions de base

---
+++ CalculatorProject/CalculatorProject/base.c
@@ -0,0 +1,26 @@
+#include "base.h"
+
+int factorielle(int x) {
+	int result = 1;
+	while (x > 1) {
+		result *= x;
+		x--;
+	}
+	return result;
+}
+
+double puissance(double x, int exposant) {
+	if (x == 0)
+		return 0;
+
+	int result = 1;
+	int i = 0;
+
+	while (i < exposant) {
+		result *= x;
+		i++;
+	}
+
+	return result;
+}
+
